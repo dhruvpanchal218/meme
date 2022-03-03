51d0c9187950b87a44e99b07ef36372a8690a0a5
Dhruv Panchal <dhruvpanchal218@gmail.com>
2022-03-03 00:27:57 -0500

    init

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import Header from "./components/header";
+import "./App.css";
+import Meme from "./components/meme";
+
+export default function App() {
+ return (
+  <div>
+   <Header />
+   <Meme />
+  </div>
+ );
+}
---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+
+export default function Header() {
+ return (
+  <header className="header">
+   <img src="./meme.png" className="header--image" />
+   <h2 className="header--title">Meme Generator</h2>
+  </header>
+ );
+}
